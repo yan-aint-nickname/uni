075f426fb19b50491192a059d49a94bb5e4f3c4f
Yan Kirsh <yankirsh@yandex.ru>
2024-03-25 15:54:23 +0300

    Add matrix type

--- oop-matrix-cli/main.cc
+++ oop-matrix-cli/main.cc
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib> // For rand() and srand()
-#include <ctime> // For time()
+#include <cstdlib>
+#include <ctime>
+
+using matrix = std::vector<std::vector<int>>;
 
 // Функция(Процедура) для заполнения матрицы рандомными значениями
-void fillMatrix(std::vector<std::vector<int> >& matrix, int rows, int cols) {
+void fillMatrix(matrix &values, int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            matrix[i][j] = rand() % 100; // Random values between 0 and 99
+            values[i][j] = std::rand() % 100; // Random values between 0 and 99
         }
     }
 }
 
 // Фнукция для подсчет кол-ва элементов больших, чем данное
-int countGreaterThan(const std::vector<std::vector<int> >& matrix, int value) {
+int countGreaterThan(const matrix &values, int value) {
     int count = 0;
-    for (const auto& row: matrix) {
+    for (const auto& row: values) {
         for (int element: row) {
             if (element > value) {
                 ++count;
@@ -26,7 +28,7 @@ int countGreaterThan(const std::vector<std::vector<int> >& matrix, int value) {
 }
 
 // Функция(Процедура) для получения данных от пользователя
-void getUserInput(int& D, int& Q) {
+void getUserInput(int &D, int &Q) {
     std::cout << "Введите число D: ";
     std::cin >> D;
     std::cout << "Введите число Q: ";
@@ -34,11 +36,11 @@ void getUserInput(int& D, int& Q) {
 }
 
 int main() {
-    srand(static_cast<unsigned>(time(0))); // Seed random number generator
+	std::srand(static_cast<unsigned>(time(0))); // Seed random number generator
 
     // Define matrices A and B
-    std::vector<std::vector<int> > A(5, std::vector<int>(8));
-    std::vector<std::vector<int> > B(7, std::vector<int>(5));
+    matrix A(5, std::vector<int>(8));
+    matrix B(7, std::vector<int>(5));
 
     // Fill matrices with random values
     fillMatrix(A, 5, 8);
