8f3b17222ad7092cbe5021856686cc0b7f07c2b5
Yan Kirsh <yankirsh@yandex.ru>
2024-03-26 22:30:41 +0300

    Made class impl and formatting

--- oop-matrix-cli/main.cc
+++ oop-matrix-cli/main.cc
@@ -1,62 +1,94 @@
-#include <iostream>
-#include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <vector>
 
 using matrix = std::vector<std::vector<int>>;
 
-// Функция(Процедура) для заполнения матрицы случаными значениями
-void fillMatrix(matrix &values, int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            values[i][j] = std::rand() % 100; // Случайные числа между 0 и 99
-        }
-    }
-}
+class Matrix {
+  private:
+    matrix values;
+    int rows, columns;
 
-// Фнукция для подсчет кол-ва элементов больших, чем данное
-int countGreaterThan(const matrix &values, int value) {
-    int count = 0;
-    for (const auto &row: values) {
-        for (int element: row) {
-            if (element > value) {
-                ++count;
+  public:
+    Matrix(int rows, int columns) : rows(rows), columns(columns) {
+        values.resize(rows, std::vector<int>(columns));
+    }
+
+    // Метод для заполнения матрицы случаными значениями
+    void fillWithRandomValues() {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                values[i][j] =
+                    std::rand() % 100; // Случайные числа между 0 и 99
             }
         }
     }
-    return count;
-}
 
-// Функция(Процедура) для получения данных от пользователя
-void getUserInput(int &D, int &Q) {
-    std::cout << "Введите число D: ";
-    std::cin >> D;
-    std::cout << "Введите число Q: ";
-    std::cin >> Q;
-}
+    // Метод для подсчет кол-ва элементов больших, чем данное
+    int countGreaterThan(int value) {
+        int count = 0;
+        for (const auto &row : values) {
+            for (int element : row) {
+                if (element > value) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+class UserInput {
+  public:
+    // Метод для получения данных от пользователя возвращает 1 если ошибка
+    static int getInput(int &D, int &Q) {
+        std::cout << "Введите число D: ";
+        std::cin >> D;
+        if (std::cin.fail()) {
+            std::cout << "Not a number " << std::endl;
+            return 1;
+        }
+
+        std::cout << "Введите число Q: ";
+        std::cin >> D;
+        if (std::cin.fail()) {
+            std::cout << "Not a number " << std::endl;
+            return 1;
+        }
+        return 0;
+    }
+};
 
 int main() {
-    std::srand(static_cast<unsigned>(time(0))); // Генератор псевдо-случайных чисел
+    std::srand(
+        static_cast<unsigned>(time(0))); // Генератор псевдо-случайных чисел
 
     // Объявить матрицы A и B
-    matrix A(5, std::vector<int>(8));
-    matrix B(7, std::vector<int>(5));
+    Matrix A(5, 8);
+    Matrix B(7, 5);
 
     // Заполнить матрицы случаныйми числами
-    fillMatrix(A, 5, 8);
-    fillMatrix(B, 7, 5);
+    A.fillWithRandomValues();
+    B.fillWithRandomValues();
 
     // Получить пользовательский ввод D и Q
     int D, Q;
-    getUserInput(D, Q);
+    int err = UserInput::getInput(D, Q);
+
+    if (err != 0) {
+        return 1;
+    }
 
     // Посчитать и вывести кол-во элементов матрицы A больше D
-    int countA = countGreaterThan(A, D);
-    std::cout << "Кол-во элементов матрицы A больше чем D = " << D << ": " << countA << std::endl;
+    int countA = A.countGreaterThan(D);
+    std::cout << "Кол-во элементов матрицы A больше чем D = " << D << ": "
+              << countA << std::endl;
 
     // Посчитать и вывести кол-во элементов матрицы B больше Q
-    int countB = countGreaterThan(B, Q);
-    std::cout << "Кол-во элементов матрицы B больше чем Q = " << Q << ": " << countB << std::endl;
+    int countB = B.countGreaterThan(Q);
+    std::cout << "Кол-во элементов матрицы B больше чем Q = " << Q << ": "
+              << countB << std::endl;
 
     return 0;
 }
